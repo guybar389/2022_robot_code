56cb2d9db3c8c3c61b1482916892e1795309c08e
Oran-Zafrani <oran121242431@gmail.com>
2022-02-23 00:06:15 +0200

    added auto command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,8 +3,17 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.links.SPILink;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,23 +26,67 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
+  
+    RobotContainer cont = new RobotContainer();
+    MecanumDrive driveTrain = cont.driveTrain;
+    Joystick driveStick = cont.driveStick;
+    Pixy2 pixy = Pixy2.createInstance(new SPILink());
+    autoGrabBall auto = new autoGrabBall(pixy);
+    PIDController turnController = new PIDController(0.1, 0, 0);
+    double ballx;
+    DigitalInput ballSwitch = cont.ballSwitch;
+    boolean ballInside = false;
+    
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    if (DriverStation.getAlliance() == Alliance.Blue){
+      auto.setBlueAlliance();
+    }
+    else if (DriverStation.getAlliance() == Alliance.Red){
+      auto.setRedAlliance();
+    }
+
+  }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    robotInit();
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+
+    //driveTrain.driveCartesian(0, 0, 0.5);
+    auto.execute();
+    ballx = SmartDashboard.getNumber("Ball X", 0.0);
+
+
+    if (!ballInside){
+      if (ballx != 0.0){
+         driveTrain.driveCartesian(0.5, 0, turnController.calculate(ballx, 0.0));
+      
+      }
+      else
+      driveTrain.driveCartesian(0, 0, 0.5);
+    }
+    else if(ballSwitch.get())
+      ballInside = true;
+  }
 
   @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    driveTrain.driveCartesian(driveStick.getY(), driveStick.getX(), driveStick.getZ());
+
+  }
 
   @Override
   public void disabledInit() {}
