6d6cba90eed8b2aa1a7e7bf1e34da92f084cbb25
Oran-Zafrani <oran121242431@gmail.com>
2022-02-25 01:08:11 +0200

    added RobotContainer as a parameter on constructor + added cylinders commands

--- src/main/java/frc/robot/ClimbingSystem.java
+++ src/main/java/frc/robot/ClimbingSystem.java
@@ -1,5 +1,20 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
 public class ClimbingSystem {
     
+    RobotContainer container;
+
+    
+    ClimbingSystem(RobotContainer Container){
+        this.container = Container;
+      }
+
+      //activate cylinders through those commands
+    void TwistLeftClimber(){container.LeftClimbSolenoid.set(Value.kForward);}
+    void StraightenLeftClimber(){container.LeftClimbSolenoid.set(Value.kReverse);}
+    
+    void TwistRightClimber(){container.RightClimbSolenoid.set(Value.kForward);}
+    void StraightenRightClimber(){container.RightClimbSolenoid.set(Value.kReverse);}
 }
--- src/main/java/frc/robot/IntakeSystem.java
+++ src/main/java/frc/robot/IntakeSystem.java
@@ -1,5 +1,25 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
 public class IntakeSystem {
+
+    RobotContainer container;
+
     
+    IntakeSystem(RobotContainer Container){
+        this.container = Container;
+      }
+
+
+
+    void openIntake(){                              
+      container.LeftIntakeSolenoid.set(Value.kForward);     
+      container.RightIntakeSolenoid.set(Value.kForward);
+      }
+    void CloseIntake(){
+      container.LeftIntakeSolenoid.set(Value.kReverse);
+      container.RightIntakeSolenoid.set(Value.kReverse);
+      }
+
 }
